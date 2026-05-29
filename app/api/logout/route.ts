7da7de386cccc6e85a504424b3e89d212f347c06
Manuel elias yahve ore huasaja <manuel.ore.27@unsch.edu.pe>
2026-05-29 14:29:08 -0500

    siuuuuu

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const url = new URL('/login', request.url);
-  const response = NextResponse.redirect(url);
+  const response = NextResponse.redirect(url, 303);
   response.cookies.delete('session_token');
   return response;
 }
